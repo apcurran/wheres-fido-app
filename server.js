12961f357c1c77157700114c01e4bf2f18f0d472
Alex Curran <alexcurran.va@gmail.com>
2025-08-05 15:07:11 -0500

    Add morgan for dev logging of API endpoints

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ import express from "express";
 
 const app = express();
 
+if (process.env.DEV_DEBUG === "true") {
+    const morgan = await import("morgan");
+    app.use(morgan.default("dev"));
+}
+
 app.listen(process.env.PORT, () => {
     console.log(
-        `Server running on port ${process.env.PORT}, debug mode ${process.env.DEV_DEBUG ? "on" : "off"}.`,
+        `Server running on port ${process.env.PORT}, debug mode is ${process.env.DEV_DEBUG ? "on" : "off"}.`,
     );
 });
