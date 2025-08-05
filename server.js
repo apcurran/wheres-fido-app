d8b0be91cda99dc084a3659837a587366d345734
Alex Curran <alexcurran.va@gmail.com>
2025-08-05 15:12:48 -0500

    Add 404 and general error handlers

--- server.js
+++ server.js
@@ -10,6 +10,34 @@ if (process.env.DEV_DEBUG === "true") {
     app.use(morgan.default("dev"));
 }
 
+// custom 404
+app.use((req, res) => {
+    res.status(404).json({
+        message: "Sorry, but this resource cannot be found.",
+    });
+});
+
+// general server error handler
+app.use((err, req, res, _next) => {
+    console.error(err.stack || err);
+
+    // check for nonsensical status codes by eliminating non ints
+    const statusCode =
+        err.status && Number.isInteger(err.status) ? err.status : 500;
+    let errorDetails = {
+        error: {
+            message: err.message || "Server error",
+            stack: "",
+        },
+    };
+
+    if (process.env.DEV_DEBUG === "true") {
+        errorDetails.error.stack = err.stack;
+    }
+
+    res.status(statusCode).json(errorDetails);
+});
+
 app.listen(process.env.PORT, () => {
     console.log(
         `Server running on port ${process.env.PORT}, debug mode is ${process.env.DEV_DEBUG ? "on" : "off"}.`,
