c2d5827cb23700851b4e41ad6296b7f5541fd376
Alex Curran <alexcurran.va@gmail.com>
2025-08-05 14:34:58 -0500

    Base `server.js` file for express setup

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+    console.log(
+        `Server running on port ${process.env.PORT}, debug mode ${process.env.DEV_DEBUG ? "on" : "off"}.`,
+    );
+});
