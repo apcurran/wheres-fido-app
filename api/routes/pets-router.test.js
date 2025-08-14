3801313ba7191e4875f012ffe133ccd683bd5ac0
Alex Curran <alexcurran.va@gmail.com>
2025-08-14 14:13:40 -0500

    Replace real test petId dependency with mocked Petfinder API HTTP response

--- api/routes/pets-router.test.js
+++ api/routes/pets-router.test.js
@@ -1,7 +1,8 @@
 import express from "express";
 import request from "supertest";
+import { MockAgent, setGlobalDispatcher, Agent } from "undici";
 
-import { describe, it } from "node:test";
+import { after, before, describe, it } from "node:test";
 import assert from "node:assert/strict";
 
 import petsRouter from "./pets-router.js";
@@ -21,10 +22,36 @@ describe("GET pets", () => {
 });
 
 describe("GET one pet", () => {
+    /** @type {import('undici').MockAgent} */
+    let agent;
+
+    before(() => {
+        agent = new MockAgent();
+        setGlobalDispatcher(agent);
+        agent.disableNetConnect();
+    });
+
     it("responds with specific pet data by id", async () => {
         const petId = 77768279;
+        const mockedName = "mocked_pet";
+        agent
+            .get(`${process.env.PETFINDER_BASE_URL}`)
+            .intercept({ path: `/v2/animals/${petId}` })
+            .reply(200, {
+                animal: {
+                    id: petId,
+                    name: mockedName,
+                },
+            });
+
         const result = await request(app).get(`/api/pets/${petId}`);
         assert.strictEqual(result.status, 200);
-        assert.ok(result.body.animal);
+        assert.strictEqual(result.body.animal.id, petId);
+        assert.strictEqual(result.body.animal.name, mockedName);
+    });
+
+    after(() => {
+        // restore default Fetch API
+        setGlobalDispatcher(new Agent());
     });
 });
