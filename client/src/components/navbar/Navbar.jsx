7cf962cecf4a2bd3bd1eb9d4329e99bd1ddfb9fc
Alex Curran <alexcurran.va@gmail.com>
2025-08-20 17:06:09 -0500

    Remove unnecessary div and add layout styles

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,17 +1,16 @@
 import { Link } from "react-router";
 
+import "./Navbar.css";
 import logo from "./logo-paw.svg";
 
 function Navbar() {
     return (
         <header className="header-nav">
             <nav>
-                <div>
-                    <Link to="/">
-                        <span>Where's Fido</span>
-                        <img src={logo} alt="Dog paw logo" />
-                    </Link>
-                </div>
+                <Link to="/" className="logo">
+                    <span>Where's Fido</span>
+                    <img src={logo} alt="Dog paw logo" />
+                </Link>
                 <ul>
                     <li>
                         <Link to="/about">About</Link>
