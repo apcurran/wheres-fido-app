80d9901855aa59d154ffb0a8a402a6dd5700c3db
Alex Curran <alexcurran.va@gmail.com>
2025-10-23 16:16:48 -0500

    Form elements skeleton

--- client/src/pages/search-pets/SearchPets.jsx
+++ client/src/pages/search-pets/SearchPets.jsx
@@ -10,8 +10,18 @@ function SearchPetsPage() {
                         <label htmlFor="zip">Enter Zip Code</label>
                         <input type="number" />
                     </div>
-                    <div className="form-group"></div>
-                    <button className="form-submit-btn">Submit</button>
+                    <div className="form-group">
+                        <label htmlFor="pet-species-select">Select Pet:</label>
+                        <select name="" id="pet-species-select">
+                            <button>
+                                <selectedcontent></selectedcontent>
+                            </button>
+
+                            <option value="dog">Dog</option>
+                            <option value="cat">Cat</option>
+                        </select>
+                    </div>
+                    <button className="form-submit-btn">Search</button>
                 </form>
             </div>
         </div>
